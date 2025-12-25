c3647b3a8d397883eef08da80a797dcd787b51a2
christianhaitian <mckman@comcast.net>
2025-12-24 23:21:18 -0500

    Initial update to fix drastic

--- dArkOSUpdate.sh
+++ dArkOSUpdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-UPDATE_DATE=""
+UPDATE_DATE="12242025"
 LOG_FILE="/home/ark/update$UPDATE_DATE.log"
 UPDATE_DONE="/home/ark/.config/.update$UPDATE_DATE"
 
@@ -13,6 +13,8 @@ fi
 if [ -f "$LOG_FILE" ]; then
 	sudo rm "$LOG_FILE"
 fi
+
+LOCATION="https://raw.githubusercontent.com/christianhaitian/darkos-updates/master"
 
 sudo msgbox "ONCE YOU PROCEED WITH THIS UPDATE SCRIPT, DO NOT STOP THIS SCRIPT UNTIL IT IS COMPLETED OR THIS DISTRIBUTION MAY BE LEFT IN A STATE OF UNUSABILITY.  Make sure you've created a backup of this sd card as a precaution in case something goes very wrong with this process.  You've been warned!  Type OK in the next screen to proceed."
 my_var=`osk "Enter OK here to proceed." | tail -n 1`
@@ -32,23 +34,21 @@ echo 255 > /sys/class/backlight/backlight/brightness
 touch $LOG_FILE
 tail -f $LOG_FILE >> /dev/tty1 &
 
-if [ ! -f "/home/ark/.config/.update????2025" ]; then
+if [ ! -f "/home/ark/.config/.update12242025" ]; then
 
-	printf "\nUpdate retrorun.cfg to fix analog stick\n" | tee -a "$LOG_FILE"
+	printf "\nAdd missing files for Drastic\nUpdate drastic.sh script\n" | tee -a "$LOG_FILE"
 	sudo rm -rf /dev/shm/*
-	sudo wget -t 3 -T 60 --no-check-certificate "$LOCATION"/????2025/arkosupdate????2025.zip -O /dev/shm/arkosupdate????2025.zip -a "$LOG_FILE" || sudo rm -f /dev/shm/arkosupdate????2025.zip | tee -a "$LOG_FILE"
-	if [ -f "/dev/shm/arkosupdate????2025.zip" ]; then
-	  sudo unzip -X -o /dev/shm/arkosupdate????2025.zip -d / | tee -a "$LOG_FILE"
-	  sudo rm -fv /dev/shm/arkosupdate????2025.zip | tee -a "$LOG_FILE"
+	sudo wget -t 3 -T 60 --no-check-certificate "$LOCATION"/12242025/darkosupdate12242025.zip -O /dev/shm/darkosupdate12242025.zip -a "$LOG_FILE" || sudo rm -f /dev/shm/darkosupdate12242025.zip | tee -a "$LOG_FILE"
+	if [ -f "/dev/shm/darkosupdate12242025.zip" ]; then
+	  sudo unzip -X -o /dev/shm/darkosupdate12242025.zip -d / | tee -a "$LOG_FILE"
+	  sudo rm -fv /dev/shm/darkosupdate12242025.zip | tee -a "$LOG_FILE"
 	else
 	  printf "\nThe update couldn't complete because the package did not download correctly.\nPlease retry the update again." | tee -a "$LOG_FILE"
-	  sudo rm -fv /dev/shm/arkosupdate????2025.z* | tee -a "$LOG_FILE"
+	  sudo rm -fv /dev/shm/darkosupdate12242025.z* | tee -a "$LOG_FILE"
 	  sleep 3
 	  echo $c_brightness > /sys/class/backlight/backlight/brightness
 	  exit 1
 	fi
-
-
 
 	printf "\nUpdate boot text to reflect current version of dArkOS\n" | tee -a "$LOG_FILE"
 	sudo sed -i "/title\=/c\title\=dArkOS ($UPDATE_DATE)" /usr/share/plymouth/themes/text.plymouth
